9af8430ac823a42800eeeeebd009359e344e59a1
evelynea <93907954+evelynea@users.noreply.github.com>
2022-09-05 09:05:32 +0200

    solo project

---
+++ solo.js
@@ -0,0 +1,17 @@
+let score = document.getElementById("score")
+let plusOne = 0
+function add1(){
+    plusOne +=1
+    score.innerText = plusOne
+    console.log(plusOne);
+}
+function add2(){
+    plusOne +=2
+    score.innerText = plusOne
+    console.log(plusOne);
+}
+function add3(){
+    plusOne +=3
+    score.innerText = plusOne
+    console.log(plusOne);
+}
